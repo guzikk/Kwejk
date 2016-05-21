a7d95ab26ee6cb66b45cdd365bdb45de79cfbca1
Krystian <krystian.guzara@gmail.com>
2016-05-21 10:06:54 +0200

    added gem: paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521070552) do
+ActiveRecord::Schema.define(version: 20160521075914) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,8 +35,12 @@ ActiveRecord::Schema.define(version: 20160521070552) do
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "mems", ["user_id"], name: "index_mems_on_user_id"
